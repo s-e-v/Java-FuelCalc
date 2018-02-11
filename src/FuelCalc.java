de548dd7293ca1c48ca8348ad0cb92e8cfe38c15
s-e-v <severino.gurashi@gmail.com>
2018-02-11 16:14:45 +0000

    Calculation button logic - Inside the caclulation button event the total is calculated by getting the text of various fields (coverting to double) - The final total is then set to empty label

--- src/FuelCalc.java
+++ src/FuelCalc.java
@@ -1,15 +1,37 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FuelCalc {
 
     private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField milesVal;
+    private JTextField vehichleVal;
     private JRadioButton USDRadioButton;
     private JRadioButton EURRadioButton;
     private JRadioButton GBPRadioButton;
     private JLabel totalCostVal;
     private JButton calculateButton;
+    private JTextField fuelPrice;
+
+    public FuelCalc() {
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String currency = "";
+                if (USDRadioButton.isSelected()){
+                    currency = "$";
+                } else if (EURRadioButton.isSelected()){
+                    currency = "€";
+                } else if (GBPRadioButton.isSelected()){
+                    currency = "£";
+                }
+                double efficiency = Double.parseDouble(milesVal.getText())/Double.parseDouble(vehichleVal.getText());
+                double total = Double.parseDouble(fuelPrice.getText())*efficiency;
+                totalCostVal.setText(currency + Double.toString(total));
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Fuel Calc");
